ce74eb192aefd66b157c5faa528ff1c49f09253f
thongvm316 <vominhthong1603@gmail.com>
2020-08-20 11:40:10 +0700

    20200820

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,88 @@
 const express = require('express');
 const router = express.Router();
 const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
+var ObjectId = require('mongoose').Types.ObjectId;
+
+
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status if require').not().isEmpty(),
+      check('skills', 'Skills is require').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      location,
+      website,
+      bio,
+      skills,
+      status,
+      githubusername,
+      youtube,
+      twitter,
+      instagram,
+      linkedin,
+      facebook,
+    } = req.body;
+
+    // Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map((skill) => skill.trim()); // Remove whitespace from both sides of a string:
+    }
+
+    // Build social object
+    profileFields.social = {};
+    if (youtube) profileFields.social.youtube = youtube;
+    if (twitter) profileFields.social.twitter = twitter;
+    if (instagram) profileFields.social.instagram = instagram;
+    if (linkedin) profileFields.social.linkedin = linkedin;
+    if (facebook) profileFields.social.facebook = facebook;
+
+    try {
+      let profile = await Profile.findOne({ user: req.user.id });
+      if (profile) {
+        // Update
+        profile = await Profile.findByIdAndUpdate(
+          { user: req.user.id },
+          { $set: profileFields },
+          { new: true }
+        ); // new: neu ko co feild, thi them moi vao obj
+
+        return res.json(profile);
+      }
+
+      // Create
+      profile = new Profile(profileFields)
+      await profile.save()
+      res.json(profile)
+
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 router.get('/me', auth, async (req, res) => {
   try {
