3805ad587dedcc216689b80aefd711ec99357132
thongvm316 <vominhthong1603@gmail.com>
2020-08-24 11:19:48 +0700

    20200824

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -209,7 +209,77 @@ router.delete('/experiance/:exp_id', auth, async (req, res) => {
     console.log(error.message);
     res.status(500).send('Server Error');
   }
-
-  // Next week -> Res: Why use JWT when register and login / Try: user login and profile
 });
+
+router.put(
+  '/education',
+  [
+    auth,
+    [
+      check('school', 'School is require').not().isEmpty(),
+      check('degree', 'Degree is require').not().isEmpty(),
+      check('fieldofstudy', 'Field of study is require').not().isEmpty(),
+      check('from', 'From day is require').not().isEmpty(),
+    ],
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    } = req.body;
+
+    const newEdu = {
+      school,
+      degree,
+      fieldofstudy,
+      from,
+      to,
+      current,
+      description,
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+      profile.education.unshift(newEdu); // The unshift() method adds new items to the beginning of an array, and returns the new length.
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
+
+router.delete('/education/:edu_id', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({ user: req.user.id });
+
+    // Get remove index
+    const removeIndex = profile.education
+      .map((item) => item.id)
+      .indexOf(req.params.edu_id); // indexOf: return position, if not return -1
+
+    profile.education.splice(removeIndex, 1) // removeIndex: position will be removed, 1: SL will be remove
+    await profile.save()
+    res.json(profile)
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+
 module.exports = router;
+
