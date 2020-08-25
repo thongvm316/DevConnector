e8974257016ee9be5358b014f31a6cffd2e7f205
thongvm316 <vominhthong1603@gmail.com>
2020-08-25 10:52:44 +0700

    20200825

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-router.get('/', (req, res) => res.send('Posts route'))
+// Model
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+router.post(
+  '/',
+  [auth, [check('text', 'Text is require').not().isEmpty()]],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    try {
+      const user = await User.findById(req.user.id).select('-password');
+      const newPost = new Post({
+        text: req.body.text,
+        name: user.name,
+        avatar: user.avatar,
+        user: req.user.id,
+      });
+
+      const post = await newPost.save()
+      res.json(post)
+    } catch (error) {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 module.exports = router;
