b6a78048dadc1e2c67246b70854b1ba5736af80e
thongvm316 <vominhthong1603@gmail.com>
2020-09-04 11:18:54 +0700

    20200904

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,29 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import { Navbar } from './components/layout/Navbar'
 import { Landing } from './components/layout/Landing'
 import { Register } from './components/auth/Register'
 import { Login } from './components/auth/Login'
+
+// Redux
+import { Provider } from 'react-redux'
+import store from './store'
+
 import './App.css';
 
 const App = () => (
-  <Router>
-    <Fragment>
-      <Navbar/>
-     <Route exact path='/' component={Landing}/>
-     <section className="container">
-        <Switch>
-          <Route exact path='/register' component={Register}/>
-          <Route exact path='/login' component={Login}/>
-        </Switch>
-     </section>
-    </Fragment>
-  </Router>  
+  <Provider store={store}>
+    <Router>
+        <Navbar/>
+      <Route exact path='/' component={Landing}/>
+      <section className="container">
+          <Switch>
+            <Route exact path='/register' component={Register}/>
+            <Route exact path='/login' component={Login}/>
+          </Switch>
+      </section>
+    </Router> 
+  </Provider> 
 )
     
 export default App;
