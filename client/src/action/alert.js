e86554fae0ebb590f4d98b84aba2c59ba7e18804
thongvm316 <vominhthong1603@gmail.com>
2020-09-08 11:20:51 +0700

    20200908

--- client/src/action/alert.js
+++ client/src/action/alert.js
@@ -1,40 +1,46 @@
-import { SET_ALERT, REMOVE_ALERT } from './types';
-import { v4 as uuidv4 } from 'uuid';
+// import { SET_ALERT, REMOVE_ALERT } from './types';
+// import { v4 as uuidv4 } from 'uuid';
 
-// export const setAlert = (msg, alertType) => (dispatch) => {
-//   const id = uuidv4();
-//   dispatch({
-//     type: SET_ALERT,
-//     payload: { msg, alertType, id },
-//   });
-// };
+// // export const setAlert = (msg, alertType) => (dispatch) => {
+// //   const id = uuidv4();
+// //   dispatch({
+// //     type: SET_ALERT,
+// //     payload: { msg, alertType, id },
+// //   });
+// // };
 
-// export const setAlert = (msg, alertType, timeout = 5000) => {
-//   return (dispatch) => {
-//     const id = uuidv4()
-//     dispatch({
+// // export const setAlert = (msg, alertType, timeout = 5000) => {
+// //   return (dispatch) => {
+// //     const id = uuidv4()
+// //     dispatch({
+// //       type: SET_ALERT,
+// //       payload: { msg, alertType, id },
+// //     });
+
+// //     setTimeout(() => {
+// //       dispatch({ type: REMOVE_ALERT, payload: id })
+// //     }, timeout);
+// //   };
+// // };
+
+// export const setAlert = (msg, alertType) => {
+//   const id = uuidv4()
+//     return  {
 //       type: SET_ALERT,
 //       payload: { msg, alertType, id },
-//     });
-
-//     setTimeout(() => {
-//       dispatch({ type: REMOVE_ALERT, payload: id })
-//     }, timeout);
-//   };
+//   }
 // };
 
-export const setAlert = (msg, alertType) => {
-  const id = uuidv4()
-    return  {
+
+import { v4 as uuidv4 } from 'uuid';
+import { SET_ALERT, REMOVE_ALERT } from './types';
+
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
+  const id = uuidv4();
+  dispatch({
     type: SET_ALERT,
-    payload: { msg, alertType, id },
-  }
-};
+    payload: { msg, alertType, id }
+  });
 
-export const removeAlert = (id) => {
-    return  {
-    type: REMOVE_ALERT,
-    payload: { id },
-  }
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout);
 };
-
--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,14 +1,16 @@
 import React, { Fragment, useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
 import { setAlert } from '../../action/alert';
+import { register } from '../../action/auth';
+import PropTypes from 'prop-types';
 
-const Register = () => {
+const Register = ({ setAlert, register, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
     password: '',
-    password2: '',
+    password2: ''
   });
 
   const { name, email, password, password2 } = formData;
@@ -16,76 +18,179 @@ const Register = () => {
   const onChange = (e) =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
 
-  const dispatch = useDispatch()
-  
   const onSubmit = async (e) => {
     e.preventDefault();
     if (password !== password2) {
-      dispatch(setAlert('Password do not match', 'danger'))
+      setAlert('Passwords do not match', 'danger');
     } else {
-      console.log(`SUCCESS`);
+      register({ name, email, password });
     }
   };
 
+  // if (isAuthenticated) {
+  //   return <Redirect to="/dashboard" />;
+  // }
+
   return (
     <Fragment>
-      <h1 className='large text-primary'>Sign Up</h1>
-      <p className='lead'>
-        <i className='fas fa-user'></i> Create Your Account
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Create Your Account
       </p>
-      <form className='form' onSubmit={(e) => onSubmit(e)}>
-        <div className='form-group'>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
           <input
-            type='text'
-            placeholder='Name'
+            type="text"
+            placeholder="Name"
+            name="name"
             value={name}
-            onChange={(e) => onChange(e)}
-            name='name'
-            required
+            onChange={onChange}
           />
         </div>
-        <div className='form-group'>
+        <div className="form-group">
           <input
-            type='email'
-            placeholder='Email Address'
+            type="email"
+            placeholder="Email Address"
+            name="email"
             value={email}
-            onChange={(e) => onChange(e)}
-            name='email'
-            required
+            onChange={onChange}
           />
-          <small className='form-text'>
+          <small className="form-text">
             This site uses Gravatar so if you want a profile image, use a
             Gravatar email
           </small>
         </div>
-        <div className='form-group'>
+        <div className="form-group">
           <input
-            type='password'
-            placeholder='Password'
-            name='password'
+            type="password"
+            placeholder="Password"
+            name="password"
             value={password}
-            onChange={(e) => onChange(e)}
-            minLength='6' 
+            onChange={onChange}
           />
         </div>
-        <div className='form-group'>
+        <div className="form-group">
           <input
-            type='password'
-            placeholder='Confirm Password'
-            name='password2'
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
             value={password2}
-            onChange={(e) => onChange(e)}
-            minLength='6'
+            onChange={onChange}
           />
         </div>
-        <input type='submit' className='btn btn-primary' value='Register' />
+        <input type="submit" className="btn btn-primary" value="Register" />
       </form>
-      <p className='my-1'>
-        Already have an account? <Link to='/login'>Sign In</Link>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
       </p>
     </Fragment>
   );
 };
 
-export default Register;
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+};
 
+// const mapStateToProps = (state) => ({
+//   isAuthenticated: state.auth.isAuthenticated
+// });
+
+export default connect(null, { setAlert, register })(Register);
+
+
+
+
+// import React, { Fragment, useState } from 'react';
+// import { useDispatch } from 'react-redux';
+// import { Link } from 'react-router-dom';
+// import { setAlert } from '../../action/alert';
+// import { register } from '../../action/auth'
+
+// const Register = () => {
+//   const [formData, setFormData] = useState({
+//     name: '',
+//     email: '',
+//     password: '',
+//     password2: '',
+//   });
+
+//   const { name, email, password, password2 } = formData;
+
+//   const onChange = (e) =>
+//     setFormData({ ...formData, [e.target.name]: e.target.value });
+
+//   const dispatch = useDispatch();
+
+//   const onSubmit = async (e) => {
+//     e.preventDefault();
+//     if (password !== password2) {
+//       dispatch(setAlert('Password do not match', 'danger'));
+//     } else {
+//       console.log(`SUCCESS`);
+//       dispatch()
+//     }
+//   };
+
+//   return (
+//     <Fragment>
+//       <h1 className='large text-primary'>Sign Up</h1>
+//       <p className='lead'>
+//         <i className='fas fa-user'></i> Create Your Account
+//       </p>
+//       <form className='form' onSubmit={(e) => onSubmit(e)}>
+//         <div className='form-group'>
+//           <input
+//             type='text'
+//             placeholder='Name'
+//             value={name}
+//             onChange={(e) => onChange(e)}
+//             name='name'
+//             required
+//           />
+//         </div>
+//         <div className='form-group'>
+//           <input
+//             type='email'
+//             placeholder='Email Address'
+//             value={email}
+//             onChange={(e) => onChange(e)}
+//             name='email'
+//             required
+//           />
+//           <small className='form-text'>
+//             This site uses Gravatar so if you want a profile image, use a
+//             Gravatar email
+//           </small>
+//         </div>
+//         <div className='form-group'>
+//           <input
+//             type='password'
+//             placeholder='Password'
+//             name='password'
+//             value={password}
+//             onChange={(e) => onChange(e)}
+//             minLength='6'
+//           />
+//         </div>
+//         <div className='form-group'>
+//           <input
+//             type='password'
+//             placeholder='Confirm Password'
+//             name='password2'
+//             value={password2}
+//             onChange={(e) => onChange(e)}
+//             minLength='6'
+//           />
+//         </div>
+//         <input type='submit' className='btn btn-primary' value='Register' />
+//       </form>
+//       <p className='my-1'>
+//         Already have an account? <Link to='/login'>Sign In</Link>
+//       </p>
+//     </Fragment>
+//   );
+// };
+
+// export default Register;
