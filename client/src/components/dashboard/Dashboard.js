e813b04688f6921758bfa7f542cb5a4f73eb1315
thongvm316 <vominhthong1603@gmail.com>
2020-09-18 12:01:43 +0700

    20200918

---
+++ client/src/components/dashboard/Dashboard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Dashboard = (props) => {
+    console.log(props)
+  return <div>Dashboard</div>;
+};
+
+Dashboard.propTypes = {};
+
+export default Dashboard;
---
+++ client/src/components/routing/PrivateRoute.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const PrivateRoute = (props) => {
+    const {
+        component: Component,
+        auth: { isAuthenticated, loading }, 
+        ...rest
+    } = props
+    return  (
+        <Route
+          {...rest}
+          render={props =>
+            !isAuthenticated && !loading ? (
+              <Redirect to='/login' />
+            ) : (
+              <Component {...props}/>
+            )
+          }
+        />
+      );
+}
+
+PrivateRoute.propTypes = {
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps)(PrivateRoute);
+
+/* Lam theo -> phan tich syntax -> test code ok -> suy doan muc dich -> phan tich lai giai phap */
+
+/* 
+Purpose: When user go to dashboard page, neu user chua login, he thong se redirect ve login page
+*/
