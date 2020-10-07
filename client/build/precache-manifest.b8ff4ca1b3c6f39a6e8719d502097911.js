5b9b80f63618c146214eb3fb5017d26adb3febff
thongvm316 <vominhthong1603@gmail.com>
2020-10-07 10:22:59 +0700

    Fix Again

---
+++ client/build/precache-manifest.b8ff4ca1b3c6f39a6e8719d502097911.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc137615bb010336b1757d3a35e72403",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f762fe1ba2fd16654138",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "579e9ca03bde478caa32",
+    "url": "/static/js/2.2fc84b7c.chunk.js"
+  },
+  {
+    "revision": "320addaff3e37295a01ab28fa4b66477",
+    "url": "/static/js/2.2fc84b7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f762fe1ba2fd16654138",
+    "url": "/static/js/main.bbb744ca.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "09559b134c396e071f2f09e020edf63d",
+    "url": "/static/media/spinner.09559b13.gif"
+  }
+]);
